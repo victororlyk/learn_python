6610d9c289ca39289e7990ee32dcbd2b4bda8b34
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-04-26 15:18:55 +0300

    working with classes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 everything in python is objects
+Class: template for ceating objects. All objects created using the same class
+will have the same characteristics. 
+Object: an instance of a class
+Instantiate:create an instance of a class
+Method: a function defined in a class
+Attribute: a variable bound to an instance of a class
--- oop.py
+++ oop.py
@@ -1,21 +1,42 @@
 class Kettle(object):
+    power_source = "electricity"
+
     def __init__(self, make, price):
         self.make = make
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 8.99)
-print(kenwood.make)  # Kenwood
-print(kenwood.price)  # 8.99
-
-kenwood.price = 13
-print(kenwood.price)  # 13
-
 hamilton = Kettle("Hamilton", 14.55)
 
-print(hamilton.price, hamilton.make)  # 14.55 Hamilton
+print(hamilton.on)  # False
+hamilton.switch_on()
+print(hamilton.on)  # True
 
-print("Models:{} = {}, {}={}".format(kenwood.make, kenwood.price,
-                                     hamilton.make, hamilton.price))
-# Models:Kenwood = 13, Hamilton=14.55
+Kettle.switch_on(kenwood)
+print(kenwood.on)  # True
+
+print("*" * 80)
+# we nay add attributes to instances
+kenwood.power = 1.5
+print(kenwood.power)  # 1.5
+
+print(Kettle.power_source)
+print(hamilton.power_source)
+print(kenwood.power_source)
+# electricity
+# electricity
+# electricity
+
+Kettle.power_source = "atomic"
+
+print(Kettle.power_source)
+print(hamilton.power_source)
+print(kenwood.power_source)
+# atomic
+# atomic
+# atomic
