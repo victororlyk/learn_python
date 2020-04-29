c9cbb149cfebc62a469e54d1c389ebc4746b33d7
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-04-29 22:53:26 +0300

    more oop way of song transfers

--- docStrings.py
+++ docStrings.py
@@ -3,15 +3,15 @@ class Song:
     class to represent a song
     """
 
-    def __init__(self, title, artist, duration=0):
+    def __init__(self, name, artist, duration=0):
         """
-        :param title: (str) Initialises the 'title' attribute
-        :param article: (Artist) At  Artist object represeting the song's
+        :param name: (str) Initialises the 'title' attribute
+        :param artist: (Artist) At  Artist object represeting the song's
         creator.
         :param duration:(Optional[int]) Initial value for the 'duration'
                 attribute. Will defualt to zero if not specified.
         """
-        self.title = title
+        self.name = name
         self.artist = artist
         self.duration = duration
 
@@ -42,15 +42,18 @@ class Album:
     def add_song(self, song, position=None):
         """
         Adds a song to the track list
-        :param song:  a song to add
+        :param song:  a title of song to add
         :param position: optional[int] if specified the song will be added
         to that position, otherwise to the end of the list
         :return: None
         """
-        if position is None:
-            self.tracks.append(song)
-        else:
-            self.tracks.insert(position, song)
+        song_found = find_object(song, self.tracks)
+        if song_found is None:
+            song_found = Song(song, self.artist)
+            if position is None:
+                self.tracks.append(song_found)
+            else:
+                self.tracks.insert(position, song_found)
 
 
 class Artist:
@@ -75,6 +78,23 @@ class Artist:
         """
         self.albums.append(album)
 
+    def add_song(self, name, year, title):
+        """
+        Add a new song to the collection of albums
+        :param name: name of the album
+        :param year: the year album was produced
+        :param title: title of the song
+        :return: None
+        """
+        album_found = find_object(name, self.albums)
+        if album_found is None:
+            print("{} not found".format(name))
+            album_found = Album(name, year, self)
+            self.add_album(album_found)
+        else:
+            print("Found album {}".format(name))
+        album_found.add_song(title)
+
 
 def find_object(field, object_list):
     """
@@ -89,45 +109,20 @@ def find_object(field, object_list):
 
 
 def load_data():
-    new_artist = None
-    new_album = None
     artist_list = []
+
     with open("albums.txt", "r", encoding='utf-8') as albums:
         for line in albums:
             # data row should consist of (artist, album, year, song)
             artist_field, album_field, year_field, song_field = tuple(
                 line.strip('\n').split('\t'))
             year_field = int(year_field)
+
+            new_artist = find_object(artist_field, artist_list)
             if new_artist is None:
                 new_artist = Artist(artist_field)
                 artist_list.append(new_artist)
-            elif new_artist.name != artist_field:
-                # we'be just read details for a new artist retireve artist
-                # object if there is one
-                # otherweise create a new artist object and add it to the
-                # artist line
-                new_artist = find_object(artist_field, artist_list)
-                if new_artist is None:
-                    new_artist = Artist(artist_field)
-                    artist_list.append(new_artist)
-                new_album = None
-
-            if new_album is None:
-                new_album = Album(album_field, year_field, new_artist)
-                new_artist.add_album(new_album)
-            elif new_album.name != album_field:
-                # we'be just read a new album for the current artist
-                # retrieve the album object if there is one
-                # otherwise create a new album object and o=sotre it in athe
-                # artists collection
-                new_album = find_object(album_field, new_artist.albums)
-                if new_album is None:
-                    new_album = Album(album_field, year_field, new_artist)
-                    new_artist.add_album(new_album)
-            # create a new song object and add it to the current albums's
-            # collections
-            new_song = Song(song_field, new_artist)
-            new_album.add_song(new_song)
+            new_artist.add_song(album_field, year_field, song_field)
 
     return artist_list
 
@@ -137,7 +132,7 @@ def create_checkfile(artist_list):
         for new_artist in artist_list:
             for new_album in new_artist.albums:
                 for new_song in new_album.tracks:
-                    print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.name}".format(
                         new_artist, new_album, new_song), file=checkfile)
 
 
