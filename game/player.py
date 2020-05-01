e93af36455749687b33c49c78cbd04772af989f6
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-05-01 09:57:54 +0300

    added decorators for properties

--- game/player.py
+++ game/player.py
@@ -3,7 +3,7 @@ class Player(object):
         self.name = name
         self._lives = 3
         self._level = 1
-        self.score = 0
+        self._score = 0
 
     def _get_lives(self):
         return self._lives
@@ -15,8 +15,6 @@ class Player(object):
             print("can't be negative")
             self._lives = 0
 
-    lives = property(_get_lives, _set_lives)  # setter and getter
-
     def _get_level(self):
         return self._level
 
@@ -24,11 +22,20 @@ class Player(object):
         if level > 0:
             delta = level - self._level
             self._level = level
-            self.score += (delta * 1000)
+            self._score += (delta * 1000)
         elif self._level > 0:
             print("level can't be less then 0")
 
+    lives = property(_get_lives, _set_lives)  # setter and getter
     level = property(_get_level, _set_level)
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        self._score = score
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score: {0.score}".format(
