93fb7840386525d195bbd5459709563cf148fb08
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-05-03 11:20:26 +0300

    working witj db

--- db/contacts2.py
+++ db/contacts2.py
@@ -4,15 +4,20 @@ db = sqlite3.connect('contacts.sqlite')
 
 # to update rows we may use strings which we can pass to cursor and then
 # even get amount of updated  rows
-update_sql = "UPDATE contacts SET email = 'updated@updated.com' WHERE " \
-             "contacts.phone = 234"
+new_email = "w@gmail.com"
+phone = input('please enter a number')  # if user would input something like
+# 1;drop table contacts if we have no execute but executescript it will
+# delete our table
+update_sql = "UPDATE contacts SET email = ? WHERE contacts.phone = ?"
+print(update_sql)
+
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+update_cursor.execute(update_sql, (new_email, phone))
+
 print("{} rows updated".format(update_cursor.rowcount))
-print(db.commit == update_cursor.connection.commit)
+
 update_cursor.connection.commit()
 update_cursor.close()
-
 
 c = db.cursor()
 c.execute("SELECT * FROM contacts")
--- db/contacts3.py
+++ db/contacts3.py
@@ -2,9 +2,12 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
+n = input("Enter a name")
 
 cursor = db.cursor()
-cursor.execute("SELECT * FROM contacts")
+cursor.execute("SELECT * FROM contacts WHERE name Like ?", (n,))
 
 for row in cursor:
     print(row)
+
+db.close()
