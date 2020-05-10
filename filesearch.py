a5d2065cd915ef5e3817b956ab057fb803b8ebe1
Viktor.Orlyk <victor.orlyk93@gmail.com>
2020-05-10 10:48:21 +0300

    searching with library

--- filesearch.py
+++ filesearch.py
@@ -1,33 +1,6 @@
 import os
 import fnmatch
-import id3reader._p3 as id3reader
-
-
-
-
-def find_albums(root, artist_name):
-    caps_name = artist_name.upper()
-    for path, directories, files in os.walk(root):
-        for artist in (d for d in directories if
-                       fnmatch.fnmatch(d.upper(), caps_name)):
-            subdir = os.path.join(path, artist)
-            for album_path, albums, _ in os.walk(subdir):
-                for album in albums:
-                    yield os.path.join(album_path, album), album
-
-
-def find_songs(albums):
-    for album in albums:
-        for song in os.listdir(album[0]):
-            yield song
-
-
-albums_list = find_albums("music", "Aerosmith")
-song_list = find_songs(albums_list)
-
-
-# for a in song_list:
-#     print(a)
+import id3reader_p3 as id3reader
 
 
 def get_all(path: str, extension: str):
@@ -37,5 +10,19 @@ def get_all(path: str, extension: str):
             yield os.path.join(absolute_path, file)
 
 
-# for f in get_all('music', 'emp3'):
-#     print(f)
+my_music = get_all('music', 'emp3')
+error_list = []
+
+for f in my_music:
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Albums:{}, Track: {}, Song: {}".format(
+            id3r.get_value('performer'),
+            id3r.get_value('album'),
+            id3r.get_value('track'),
+            id3r.get_value('title')
+        ))
+    except OSError:
+        error_list.append(f)
+        continue
+print(error_list)
